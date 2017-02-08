9ce9855dfc0a7724eda926c5a407f0fc0097929c
rslisenko <rvslisenko@gmail.com>
2017-02-08 14:54:00 +0200

    commit

---
+++ src/assets/js/app.js
@@ -0,0 +1,38 @@
+//=require jquery.min.js
+
+// to inlude bootstrap plugins add "=" sign below
+// require javascripts/bootstrap/*.*
+// require javascripts/bootstrap.min.js
+
+//=require slick.min.js
+
+;(function($){
+	$(document).ready(function(){
+		var $slider = $('.ba-slider');
+
+		$slider.slick({
+			dots: true,
+			slide: ".ba-slider__item",
+			prevArrow: ".ba-slider__prev",
+			nextArrow: ".ba-slider__next",
+			customPaging : function(slider, i) {
+				var slides = $(slider.$slides[i]).data();
+				return '<a>' + '0' + (i + 1) + '</a>';
+			},
+			responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					dots: false
+				}
+			},
+			]
+		});
+		$('.current-slide').text('01');
+		$('.ba-slider').on('afterChange', function(event, slick, currentSlide){
+			$('.current-slide').text('0' + (currentSlide + 1));
+		});
+	});
+})(jQuery);
